22ae026ede7b4df754c9d31a015bdb181d030c64
Lucas Barros <lucasbarros689@gmail.com>
2017-09-18 21:28:31 -0300

    Corrigindo arquivos html e criando views

--- web/project/views.py
+++ web/project/views.py
@@ -7,3 +7,55 @@ from project.models import *
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/blank')
+def blank():
+    return render_template('blank.html')
+
+@app.route('/buttons')
+def buttons():
+    return render_template('buttons.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/flot')
+def flot():
+    return render_template('flot.html')
+
+@app.route('/morris')
+def morris():
+    return render_template('morris.html')
+
+@app.route('/forms')
+def forms():
+    return render_template('forms.html')
+
+@app.route('/notifications')
+def notifications():
+    return render_template('notifications.html')
+
+@app.route('/grid')
+def grid():
+    return render_template('grid.html')
+
+@app.route('/panels-wells')
+def panels_wells():
+    return render_template('panels-wells.html')
+
+@app.route('/homepage')
+def homepage():
+    return render_template('homepage.html')
+
+@app.route('/tables')
+def tables():
+    return render_template('tables.html')
+
+@app.route('/icons')
+def icons():
+    return render_template('icons.html')
+
+@app.route('/typography')
+def typography():
+    return render_template('typography.html')
